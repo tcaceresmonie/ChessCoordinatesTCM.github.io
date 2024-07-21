d7359e0b69527a86a054f4c55092560a765d2da1
tcaceresmonie <144068572+tcaceresmonie@users.noreply.github.com>
2024-07-21 18:18:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
+var letterSquare = ["a","b","c","d","e","f","g","h"];
+
+
 var allCoordinates = [];
 var userAnswers = [];
-var letterSquare = ["a","b","c","d","e","f","g","h"];
 var nextCoordinate = -1;
 var rightAnswers = 0;
-
-
 var started = false;
+var timer;
+var timeLeft= 30;
+
+
 startTheGame ();
+
 
 function startTheGame(){
     $("h1").on("click", function(){
         if (!started){
+            started = true;
+            allCoordinates = [];
+            userAnswers = [];
+            nextCoordinate = -1;
+            rightAnswers = 0;
+            timeLeft = 30;
             countdown();
-            started = true;}
-    })}
+            allowClick ();
+            $("h2").text("Time:30s")
+        }})}
 
 
 function countdown() {
@@ -25,6 +37,7 @@ function countdown() {
             $("h1").text("1");
             setTimeout(function() {
                 newCoordinate();
+                startTimer();
             }, 1000);
         }, 1000);
     }, 1000);
@@ -32,6 +45,8 @@ function countdown() {
 
 
 function newCoordinate (){
+    
+    
     nextCoordinate++;
 
     var randomNumber = Math.floor (Math.random()*8)+1;
@@ -47,8 +62,26 @@ function newCoordinate (){
     $("h1").text (randomCoordinate);
 }
 
+function startTimer() {
+    clearInterval(timer);
+    timer = setInterval(function () {
+        timeLeft--;
+        if (timeLeft <= 0) {
+            gameOver("Time's Up! Click Again to Restart");
+        }
+        $("h2").text("Time Left: " + timeLeft + "s");
+    }, 1000);
+}
+
+
+
 //USER CLICK
-$(".square").on("click", function(event){
+function allowClick (){
+
+    $(".square").off("click");
+    if (started === true){
+
+    $(".square").on("click", function(event){
 
     var userClickedSquare = this.id;
     userAnswers.push (userClickedSquare);
@@ -56,8 +89,8 @@ $(".square").on("click", function(event){
     $("#"+userClickedSquare).addClass("pressed");
     setTimeout(function (){
     $("#"+userClickedSquare).removeClass ("pressed");
-    }, 100)
-})
+    }, 100)}
+)}}
 
 
 
@@ -69,15 +102,18 @@ function checkAnswer(){
         newCoordinate();
 
     }else{
-        started = false;
-        $("h1").text("Game Over, Click Again to Restart");
-        $("body").removeClass("gradient");
-        setTimeout(function(){$("body").addClass("gradient")}, 200);
-        startOver();
+        gameOver();
     }
 }
 
-function startOver(){
-    var allCoordinates = 0;
-    var userAnswers = 0;
+
+function gameOver (){
+    started = false;
+    clearInterval(timer);
+        $("h1").text("Game Over, Click Here Restart");
+        $("body").removeClass("gradient");
+        setTimeout(function(){$("body").addClass("gradient")}, 200);
 }
+
+
+
